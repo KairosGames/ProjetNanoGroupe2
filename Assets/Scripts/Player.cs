bb98b3d558ee2c8928c6bfed6b1d60edc1dedb9c
Ydris <ydris.benmansour@studioblackflag.com>
2025-12-08 12:38:55 +0100

    add-respawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,11 @@ public class Player : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] TrackRenderer trackRenderer;
-    [SerializeField] GameObject childVisual;
     [SerializeField] Player otherPlayer;
+
+    [Header("InterReferences")]
+    [SerializeField] GameObject[] activeOnTrack;
+    [SerializeField] GameObject childVisual;
 
     [Header("Settings")]
     [Tooltip("Must be 0 or 1")]
@@ -22,8 +25,9 @@ public class Player : MonoBehaviour
     [SerializeField] LayerMask boostLayerBoth;
     [SerializeField] float fallingSpeed = 5.0f;
     [SerializeField] float jumpTime = 0.25f;
+    [SerializeField] float respawnTime = 1.0f;
 
-    [System.NonSerialized] public int maxOffset = 1;
+    [System.NonSerialized] public int maxOffset = 2;
     [HideInInspector] public bool isBoosting = false;
     
 
@@ -36,12 +40,16 @@ public class Player : MonoBehaviour
     bool isReady = false;
     bool isJumping = false;
     bool isFalling = false;
+    bool isRespawning = false;
+    bool canChoose = false;
+    float localYResetSpawn = 2.0f;
 
 
     [Header("MovementSmoothness")]
     [SerializeField] float tWeight = 0.08f;
     float lastT = 0.0f;
     float tVel = 0.0f;
+
 
     private void Awake()
     {
@@ -54,19 +62,28 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        SetActiveOnTrack();
+
         if (!isReady)
             return;
 
         bool isBoostActionPressed = playerId == 0 ? Input.GetKey(KeyCode.Joystick1Button0) : Input.GetKey(KeyCode.Joystick2Button0);
+        bool isChooseActionJustPressed = playerId == 0 ? Input.GetKeyDown(KeyCode.Joystick1Button0) : Input.GetKeyDown(KeyCode.Joystick2Button0);
         bool isJumpActionReleased = playerId == 0 ? Input.GetKeyUp(KeyCode.Joystick1Button0) : Input.GetKeyUp(KeyCode.Joystick2Button0);
         inputDir = Input.GetAxis($"Horizontal_P{playerId + 1}");
         inputDir = Mathf.Abs(inputDir) >= 0.6f ? Mathf.Sign(inputDir) : 0.0f;
 
-        if (!isFalling)
-            SetPosition();
+        if (isRespawning)
+        {
+            ChooseLane(isChooseActionJustPressed);
+            return;
+        }
 
         if (!isJumping && !isFalling)
+        {
+            SetPosition();
             CheckGround();
+        }
 
         if (isFalling)
             Fall();
@@ -76,6 +93,13 @@ public class Player : MonoBehaviour
 
         if (!isJumping && !isFalling && isJumpActionReleased)
             LaunchJump(); 
+    }
+
+
+    void SetActiveOnTrack()
+    {
+        foreach (GameObject go in activeOnTrack)
+            go.SetActive(!isJumping && !isFalling && !isRespawning);
     }
 
 
@@ -100,6 +124,7 @@ public class Player : MonoBehaviour
             return;
 
         isFalling = true;
+        StartCoroutine(LaunchRespawnTimer());
     }
 
 
@@ -159,12 +184,47 @@ public class Player : MonoBehaviour
         actualOffset += (int)inputDir;
         lastT = 0.0f;
 
-        /*Tween.LocalPositionX(
+        Tween.LocalPositionX(
             transform,
             transform.localPosition.x,
             transform.localPosition.x + (offsetLen * inputDir),
             jumpTime,
-            ease: Ease.OutQuart);*/
+            ease: Ease.OutQuart);
+    }
+
+
+    void ChooseLane(bool isChooseActionJustPressed)
+    {
+        if (isChooseActionJustPressed && canChoose)
+        {
+            canChoose = false;
+
+            Tween.LocalPositionY(
+            transform,
+            transform.localPosition.y,
+            0.0f,
+            0.4f,
+            ease: Ease.InQuart
+            ).OnComplete(() => { isRespawning = false; });
+
+            return;
+        }
+
+        if (canChoose && inputDir != 0.0f)
+        {
+            if (Mathf.Abs(actualOffset + inputDir) > maxOffset)
+                return;
+
+            actualOffset += (int)inputDir;
+            canChoose = false;
+
+            Tween.LocalPositionX(
+            transform,
+            transform.localPosition.x,
+            transform.localPosition.x + (offsetLen * inputDir),
+            jumpTime,
+            ease: Ease.OutQuart).OnComplete(() => { canChoose = true; });
+        }
     }
 
 
@@ -172,5 +232,24 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         isReady = true;
+    }
+
+
+    IEnumerator LaunchRespawnTimer()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        isFalling = false;
+        isRespawning = true;
+        actualOffset = 0;
+        lastT = 0.0f;
+        transform.localPosition = new Vector3(0.0f, 6.0f, 0.0f);
+
+        Tween.LocalPositionY(
+            transform,
+            transform.localPosition.y,
+            localYResetSpawn,
+            0.5f,
+            ease: Ease.OutQuint
+            ).OnComplete(() => { canChoose = true; });
     }
 }
